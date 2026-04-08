0882aba90a228202e64fc6d14fb698d118c0c814
uchqunaliyev0-svg <SAYTINGDAGI_EMAIL>
2026-04-08 02:46:22 -0700

    Fixed TOKEN naming and import error

--- config.py
+++ config.py
@@ -1,1 +1,4 @@
-BOT_TOKEN = "8725722196:AAFJ7-ADisGy_n8hPQph3yVhsGMCk9pSXBo"
+import os
+
+# Railway Variables'dan qidiradi, topolmasa kod ichidagini oladi
+TOKEN = os.getenv("TOKEN", "8725722196:AAFJ7-ADisGy_n8hPQph3yVhsGMCk9pSXBo")
