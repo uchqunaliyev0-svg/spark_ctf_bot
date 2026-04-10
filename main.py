8961c6965871ea61b1e1f230b0ea262bd02194fd
uchqunaliyev0-svg <SAYTINGDAGI_EMAIL>
2026-04-10 00:51:58 -0700

    update

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 import asyncio
 import logging
-import matplotlib
-matplotlib.use('Agg')  # Railway uchun shart!
 from aiogram import Bot, Dispatcher
 from config import TOKEN
 from handlers import start, profile, ranking, tasks, info, admin
 
+# Botingni eski modullarini ulaymiz
 async def main():
-    # Bot va Dispatcher
     bot = Bot(token=TOKEN)
     dp = Dispatcher()
 
-    # Sening papkangdagi routerlarni ulash
-    # Har bir fayling ichida 'router' bor deb hisoblaymiz (standard aiogram 3)
+    # Sening handlers papkangdagi routerlarni ulaymiz
     dp.include_router(start.router)
     dp.include_router(profile.router)
     dp.include_router(ranking.router)
@@ -20,10 +17,10 @@ async def main():
     dp.include_router(info.router)
     dp.include_router(admin.router)
 
-    print("⚡️ @SparkCTF_bot barcha modullari bilan ishga tushdi...")
-    
-    # Eski sessiyalarni tozalash (Conflict bo'lmasligi uchun)
+    # Eski kutilayotgan xabarlarni tozalaymiz
     await bot.delete_webhook(drop_pending_updates=True)
+    
+    print("⚡️ SparkCTF Bot eski zabardast holatiga qaytdi!")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
@@ -31,4 +28,4 @@ if __name__ == "__main__":
     try:
         asyncio.run(main())
     except (KeyboardInterrupt, SystemExit):
-        print("Bot to'xtatildi")
+        pass
