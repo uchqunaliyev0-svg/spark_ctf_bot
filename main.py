6cf23e99f8085ea88825a3061f122e4e01df2c89
root <root@picoctf>
2026-04-21 04:00:46 -0700

    Fix router order for FSM state cancelling

--- main.py
+++ main.py
@@ -5,21 +5,23 @@ from handlers import start, profile, tasks, ranking, admin, info, rename
 from database import init_db
 from middlewares.throttling import ThrottlingMiddleware
 
+from config import TOKEN
+
 async def main():
-    bot = Bot(token=os.getenv("BOT_TOKEN"))
+    bot = Bot(token=TOKEN)
     dp = Dispatcher()
     
     # ANTI-FLOOD YOQILDI
     dp.message.middleware(ThrottlingMiddleware(limit=1.0))
     
     await init_db()
-    dp.include_router(admin.router)
     dp.include_router(start.router)
     dp.include_router(profile.router)
     dp.include_router(tasks.router)
     dp.include_router(ranking.router)
     dp.include_router(info.router)
     dp.include_router(rename.router)
+    dp.include_router(admin.router)
 
     print("🚀 SPARK CTF SECURE SYSTEM IS LIVE!")
     await dp.start_polling(bot)
